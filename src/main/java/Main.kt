b62d5dbc2518e92d8affc57f6e20198029701220
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 23:37:46 +0200

    We are functionals, we don't need classes

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,7 +1,5 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-class Connector {
-    fun connect(host: String = "localhost", port: Int = 80) {
-        //connection logic
-    }
+fun connect(host: String = "localhost", port: Int = 80) {
+    //connection logic
 }
