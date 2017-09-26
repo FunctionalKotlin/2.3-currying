1c05dca7c5dc2029ae07bf86c61dfd124ed88c2c
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 23:39:13 +0200

    Currying!!

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,5 +1,14 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-fun connect(host: String = "localhost", port: Int = 80) {
-    //connection logic
+fun connect(host: String): (Int) -> Unit = { port ->
+    println("Connecting to $host:$port")
+}
+
+fun main(args: Array<String>) {
+    connect("localhost")(80)
+
+    val localConnector = connect("localhost")
+
+    localConnector(80)
+    localConnector(8888)
 }
