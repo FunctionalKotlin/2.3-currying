70db420754f1e3afae6701772fbae862b5827ecd
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 23:41:28 +0200

    More loggers? Please...

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,33 +1,5 @@
 // Copyright © FunctionalKotlin.com 2017. All rights reserved.
 
-class Connector(val host: String?, val port: Int?) {
-    fun connect() {
-        println("Connecting to $host:$port")
-    }
-}
-
-class ConnectorBuilder {
-    var connector = Connector(null, null)
-
-    fun setHost(host: String): ConnectorBuilder {
-        connector = Connector(host, connector.port)
-
-        return this
-    }
-
-    fun setPort(port: Int): ConnectorBuilder {
-        connector = Connector(connector.host, port)
-
-        return this
-    }
-
-    fun build(): Connector = connector
-}
-
-fun main(args: Array<String>) {
-    ConnectorBuilder()
-        .setHost("localhost")
-        .setPort(80)
-        .build()
-        .connect()
+fun consoleLogger(type: String, msg: String) {
+    println("[$type]    $msg")
 }
