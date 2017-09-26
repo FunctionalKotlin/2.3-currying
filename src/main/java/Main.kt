b570a6e326200ec8508f0b80ced043b604fcc3b1
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 23:42:32 +0200

    Oh, now we have a meaningless operation

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -3,3 +3,21 @@
 fun consoleLogger(type: String, msg: String) {
     println("[$type]    $msg")
 }
+
+fun operation(a: Int, b: Int, logger: (String, String) -> Unit) {
+    if (a <= 0) {
+        logger("ERROR", "a must be positive")
+        return
+    }
+
+    if (b <= 0) {
+        logger("ERROR", "b must be positive")
+        return
+    }
+
+    println("I'm doing something with $a and $b")
+}
+
+fun main(args: Array<String>) {
+    operation(1, 2, ::consoleLogger)
+}
